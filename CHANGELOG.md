85fa5784e4461d53a4114ced4df8637f7730e0bc
Bruno Sutic <bruno.sutic@gmail.com>
2014-08-30 21:11:51 +0200

    Update default offline icon for linux

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ### master
 - README updates
 - update readme to reflect github organization changes
+- linux can't display default offline icon - update it
 
 ### v0.0.2, 2014-06-02
 - bugfix: offline status icon wasn't displaying properly
--- scripts/online_status_icon.sh
+++ scripts/online_status_icon.sh
@@ -5,10 +5,32 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 online_option_string="@online_icon"
 offline_option_string="@offline_icon"
 
-online_icon_default="✔ "
-offline_icon_default="❌ "
+online_icon_osx="✅ "
+online_icon="✔ "
+offline_icon_osx="⛔️ "
+offline_icon="❌ "
 
 source $CURRENT_DIR/shared.sh
+
+is_osx() {
+	[ $(uname) == "Darwin" ]
+}
+
+online_icon_default() {
+	if is_osx; then
+		echo "$online_icon_osx"
+	else
+		echo "$online_icon"
+	fi
+}
+
+offline_icon_default() {
+	if is_osx; then
+		echo "$offline_icon_osx"
+	else
+		echo "$offline_icon"
+	fi
+}
 
 online_status() {
 	ping -c 3 www.google.com >/dev/null 2>&1
@@ -18,9 +40,9 @@ print_icon() {
 	# spacer fixes weird emoji spacing
 	local spacer=" "
 	if $(online_status); then
-		printf "$(get_tmux_option "$online_option_string" "$online_icon_default")$spacer"
+		printf "$(get_tmux_option "$online_option_string" "$(online_icon_default)")$spacer"
 	else
-		printf "$(get_tmux_option "$offline_option_string" "$offline_icon_default")$spacer"
+		printf "$(get_tmux_option "$offline_option_string" "$(offline_icon_default)")$spacer"
 	fi
 }
 
