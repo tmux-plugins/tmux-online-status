d875b2a1706a389af0382fd8d327a475b31298eb
KFBI1706 <kasper1706.f.bernhoff@gmail.com>
2016-10-23 07:22:19 +0200

    That line was triggering my OCD

--- scripts/GW.sh
+++ scripts/GW.sh
@@ -8,7 +8,6 @@ ping_timeout_string="@ping_timeout"
 route_to_ping_string="@route_to_ping"
 
 online_icon_osx="✅ "
-
 offline_icon_osx="⛔️ "
 online_icon="#[fg=green]ok##[fg=white]"
 offline_icon="#[fg=red]down##[fg=white]"
